3bcf34c60818d6c1e5a509af34fda378968ed84b
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-28 21:52:48 -0800

    Add VideoIterable class.

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import cv2
 
-capture = cv2.VideoCapture('original.mov')
+class VideoIterable(object):
+  """ Take a filename of a video and yield the current frame on each call to next() """
+  def __init__(self, filename):
+    self.filename = filename
+    capture = cv2.VideoCapture(filename)
+    read_from(capture)
 
+  def read_from(capture):
+    self.success, self.image = capture.read()
+
+  def next(self):
+    read_from(capture)
+    if (self.success == False):
+      raise StopIteration 
+    return self.image
+
+capture = cv2.VideoCapture('original.mov')
 success,image = capture.read()
 
 while success:
