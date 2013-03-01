a0289dd1e1b8c7d2cc82d36b1c8beff14f87a8a3
Tobi Lehman <tobi.lehman@gmail.com>
2013-02-28 21:24:52 -0800

    Add main loop

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import cv2
 
-while (success,image = capture.read())[0]:
+capture = cv2.VideoCapture('original.mov')
+
+success,image = capture.read()
+
+while success:
+  success,image = capture.read()
   cv2.imshow('video', image)
+  if cv2.waitKey(10) == 27:
+    break
 
